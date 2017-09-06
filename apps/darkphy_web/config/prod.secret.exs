f3f5b651c2c4c7e8ed9e1444f85d70a680d4f6e1
Slim Shady <dsslimshaddy@gmail.com>
2017-09-06 21:18:26 +0200

    ci test

---
+++ apps/darkphy_web/config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :darkphy_web, DarkphyWeb.Endpoint,
+  secret_key_base: "9KeObBK8IaXote6hcIeJwkrPFXhjAU1g87Kck7aU83iUpaykxMUaK6QFuUD0KH2u"
